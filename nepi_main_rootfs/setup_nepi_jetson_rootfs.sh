3715e72626b48e3ac9da094a5c12f3387ec3528a
Josh Maximoff <jmaximoff@numurus.com>
2023-12-08 13:28:57 -0500

    Make EMMC+SSD vs SSD-only a jetson-level build step rather than S2X
    During Vecow image development, it became clear that this decision is at least as generic as the Jetson level -- perhaps even belongs at top level.

--- nepi_main_rootfs/setup_nepi_jetson_rootfs.sh
+++ nepi_main_rootfs/setup_nepi_jetson_rootfs.sh
@@ -19,11 +19,34 @@ sudo ln -s /usr/lib/aarch64-linux-gnu/cmake/opencv4 /usr/share/OpenCV
 # Clean up anything that jetpack puts on the Desktop
 rm /home/nepi/Desktop/*
 
-# Install Zed SDK 3.8 (for Jetpack 4.6.2 - update wget command below for other versions)
-# TODO: Maybe this should be optional with user prompt -- it is quite a large install and takes a long time.
-mkdir tmp && cd tmp
-wget https://download.stereolabs.com/zedsdk/3.8/l4t32.7/jetsons
-chmod a+x jetsons
-yes Y | ./jetsons # Accept all defaults. TODO: Maybe should let the user do it interactively?
-cd ..
-rm -rf ./tmp
+read -p "Does this device have EMMC (Xavier NX, etc) or not (Orin NX, etc)? Enter y/n:" response
+case "$response" in
+    [yY][eE][sS]|[yY]
+        HAS_EMMC=Y
+    ;;
+    [nN][oO]|[nN]
+        HAS_EMMC=N
+    ;;
+    *)
+        echo "Invalid response: " + $response
+        exit 1
+    ;;
+esac
+
+# Copy the Jetson-specialized Linux config files
+sudo cp -r ${HOME_DIR}/config_jetson/* /opt/nepi/config
+
+# Update fstab - new file depends on whether this S2X has init rootfs on EMMC or NVME
+sudo mv /etc/fstab /etc/fstab.bak
+if [ $HAS_EMMC = 'Y' ]; then
+    # NEPI Storage (SSD partition 3)
+    sudo ln -sf /opt/nepi/config/etc/fstab_emmc /etc/fstab
+else
+    # NEPI Storage (SSD partition 4)
+    sudo ln -sf /opt/nepi/config/etc/fstab_nvme_only /etc/fstab
+
+# Set ownership and permissions properly - Awkardly, samba seems to use a mixed bag of samba and 
+# system authentication, but the following works
+sudo mount /mnt/nepi_storage
+sudo chown -R nepi:sambashare /mnt/nepi_storage
+sudo chmod -R 0775 /mnt/nepi_storage
--- nepi_main_rootfs/setup_nepi_s2x_rootfs.sh
+++ nepi_main_rootfs/setup_nepi_s2x_rootfs.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-read -p "Does this device have EMMC (Xavier, etc) or not (Orin, etc)? Enter y/n:" response
-case "$response" in
-    [yY][eE][sS]|[yY]
-        HAS_EMMC=Y
-    ;;
-    [nN][oO]|[nN]
-        HAS_EMMC=N
-    ;;
-    *)
-        echo "Invalid response: " + $response
-        exit 1
-    ;;
-esac
-
 # S2X-specific NEPI rootfs setup steps. This is a specialization of the NEPI Jetson rootfs
 # and calls that parent script as a pre-step.
 
@@ -26,18 +12,4 @@ HOME_DIR=$PWD
 # Copy the S2X-specialized Linux config files
 sudo cp -r ${HOME_DIR}/config_s2x/* /opt/nepi/config
 
-# Update fstab - new file depends on whether this S2X has init rootfs on EMMC or NVME
-sudo mv /etc/fstab /etc/fstab.bak
-if [ $HAS_EMMC = 'Y' ]; then
-    # NEPI Storage (SSD partition 3)
-    sudo ln -sf /opt/nepi/config/etc/fstab_emmc /etc/fstab
-else
-    # NEPI Storage (SSD partition 4)
-    sudo ln -sf /opt/nepi/config/etc/fstab_nvme_only /etc/fstab
-
-# Set ownership and permissions properly - Awkardly, samba seems to use a mixed bag of samba and 
-# system authentication, but the following works
-sudo mount /mnt/nepi_storage
-sudo chown -R nepi:sambashare /mnt/nepi_storage
-sudo chmod -R 0775 /mnt/nepi_storage
 
